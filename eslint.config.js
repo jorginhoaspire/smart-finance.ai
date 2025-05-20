9a8dd1cfc6332b6e344458e35fc1c7cf96e624e1
jorginhoaspire <jorgehenrique@softwareeagle.com.br>
2025-05-20 12:55:01 -0300

    fix: corrigido .eslintignore para o formato novoe

---
+++ eslint.config.js
@@ -0,0 +1,12 @@
+export default [
+  {
+    ignores: [
+      'node_modules',
+      'dist',
+      'build',
+      '*.min.js',
+      'tailwind.config.ts',
+    ],
+  },
+  // ... outros configs aqui
+]
